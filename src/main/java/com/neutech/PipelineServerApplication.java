6601e8e17ba2e07aa3df9429f831942f0418be0b
pzs616358428 <616358428@qq.com>
2024-05-15 20:04:41 +0800

    项目初始化

---
+++ src/main/java/com/neutech/PipelineServerApplication.java
@@ -0,0 +1,13 @@
+package com.neutech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineServerApplication.class, args);
+    }
+
+}
